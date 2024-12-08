14388f3738bcc114f65399bdb9fc34c93d571c4c
manorath901 <learn4833@gmail.com>
2024-12-08 23:38:12 +0530

    add

---
+++ script.js
@@ -0,0 +1,36 @@
+// let obj = {};
+
+// // Using an array as a key
+// let arr = [1, 2, 3];
+// obj[arr] = "Array as key";
+
+// console.log(obj); // Output: { '1,2,3': 'Array as key' }
+// console.log(obj["1,2,3"]); // Output: Array as key
+// // console.log(obj."1,2,3"); // Output: Array as key
+// console.log([1,2,3] in obj); // Output: Array as key 
+// var take function scope 
+// debugger
+// // console.log(a) //temporal dead effect 
+// let a=45;
+// let b=55;
+// console.log(ab())
+// function ab(){
+//     const x=6;
+//     const y=6;
+//     {
+//         let k=4;
+//         var m=5;
+//     }
+//     function add(u,v){
+//         console.log("hi")
+//         console.log(x)
+//         return u+v
+//     }
+//     console.log(add(5,7))
+//     return x+y;
+// }
+debugger
+
+for(var aamanub=0;aamanub<=5;aamanub++){
+    console.log(aamanub);
+}
